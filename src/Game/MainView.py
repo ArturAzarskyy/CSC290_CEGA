19e5f07f7cdaaa4d1ab7b5808b71340510c47c9e
Artur Azarskyy <artur.azarskyy@mail.utoronto.ca>
2019-10-25 19:43:59 -0400

    Improved functionality of drawing blocks
    Before the feature to draw the moving block all
    of the elements were redrawn.
    
    After the change it only fills previous place where the
    block was with the color of the background. Thus saving
    improving the runtime.

--- src/Game/MainView.py
+++ src/Game/MainView.py
@@ -7,6 +7,10 @@ pygame.init()
 
 # We can create a list of shape cordinates
 def run_game():
+    game = MainModel.MainModel()
+    controller = MainController.MainController(game)
+
+
     game_height = 800 # Size of the squeares will be aproximetly 40
     game_width =  400 # It is based that the grid is 10  * 20
     screen_width = 600
@@ -19,17 +23,18 @@ def run_game():
 
     curr_y = 40 # The bottom of the block
     curr_x = start_x
-    #Till here
 
-    game = MainModel.MainModel()
-    controller = MainController.MainController(game)
+    previous_position = None
+    next_position = [(0, 0), (160, 0), (160, 40), (0, 40)] #this is for the cordinates of the block currently this is
+    #used as example
+    #Till here are suposed to be recived from model
 
     win = pygame.display.set_mode((screen_width, game_height))
 
+    pygame.draw.line(win, (0, 255, 0), (game_width, 0), (game_width, game_height), 1)
     isRunning = True
     while isRunning:
         pygame.time.delay(1900)# this value will be received from the model
-        win.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 isRunning = False
@@ -38,18 +43,39 @@ def run_game():
 
     # we would probably get position in the grid form the model and multiply every coordinate by the size of the block
 
-        pygame.draw.line(win,(0,255,0), (game_width,0), (game_width, game_height),1)
-        pygame.draw.polygon(win, (255, 0, 0), [(0, curr_y - 40), (160, curr_y - 40), (160, curr_y), (0, curr_y)])
+        previous_position = draw_block(win, previous_position, next_position)
+        # pygame.draw.polygon(win, (255, 0, 0), [(0, curr_y - 40), (160, curr_y - 40), (160, curr_y), (0, curr_y)])
 
         if curr_y != 800:
             curr_y += 40
-            print(curr_y)
+            print(curr_y) #this functionality will be in model class
+
+        next_position = [(0, curr_y - 40), (160, curr_y - 40), (160, curr_y), (0, curr_y)]
 
         pygame.display.update()
     # This is just me experimenting with movement of the block down to see how it might work
     # Feel free to un-comment this small portion and see how it works
 
     pygame.quit()
+
+def draw_block(pygame_window, previous_position, new_position):
+    """
+    This function imitates the effect that the block is falling.
+
+    This is achieved by drawing previous position in color of background.
+    :param pygame_window: enviorment where to draw the the block
+    :param previous_position: Array of (x, y) coordinates going clockwise. Which indicate the last position of the block
+    :param new_position: Array of (x, y) coordinates  going clockwise. Which represent where to draw the block next
+    :return: Array of (x, y) points used 
+    """
+    if previous_position is None:
+        pygame.draw.polygon(pygame_window, (255,0,0), new_position)
+    else:
+        pygame.draw.polygon(pygame_window, (0,0,0), previous_position)
+        pygame.draw.polygon(pygame_window, (255,0,0), new_position)
+
+    return new_position
+
 
 if __name__ == "__main__":
     run_game()
