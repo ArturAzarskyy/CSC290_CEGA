7902e4bbbeddb4135707e4a78fd1adaf34acd764
danielapushkinsky <createsdaniel@gmail.com>
2019-10-26 19:32:28 -0400

    Revert "Added function to draw grid, as well as drawing the current level and score."
    This reverts commit 17ed8293

--- src/Game/MainModel.py
+++ src/Game/MainModel.py
@@ -1,12 +1,6 @@
+
+
 class MainModel:
     def __init__(self):
         self.curr_x_pos = 4
         self.curr_y_pos = 1
-        self.level = 1
-        self.score = 0
-
-    def get_level(self):
-        return self.level
-
-    def get_score(self):
-        return self.score
