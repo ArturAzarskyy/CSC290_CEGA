8a5f4044e32f1703b2b3d8375d9349ce92c5101d
Artur Azarskyy <artur.azarskyy@mail.utoronto.ca>
2019-11-09 19:38:59 -0500

    Hocked up controller to the files
    Controller is now hocked up correctly
    to both model and view.

--- src/Game/MainController.py
+++ src/Game/MainController.py
@@ -6,11 +6,35 @@ class MainController:
         self.game = game
 
 
-    def read_event(self,event ):
+    def read_event(self, event ):
+        """
+        Method used to determine what should be done when user precessed
+        keyboard key.
 
-
+        :param event: pygame event
+        :return: None
+        """
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                self._move(-1)
+            elif event.key == pygame.K_d:
+                self._move(1)
+            elif event.key == pygame.K_q:
+                self._rotate(-1)
+            elif event.key == pygame.K_e:
+                self._rotate(1)
         return None
 
 
-    def _rotate(self, emount, derection):
+    def _rotate(self, direction):
+        # if direction == -1:
+            # self.game.move_block_left()
+        # else:
+            # self.game.move_block_right()
         return None
+
+    def _move(self, direction):
+        if direction == -1:
+            self.game.move_block_left()
+        else:
+            self.game.move_block_right()
